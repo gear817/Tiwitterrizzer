08174300d04c5eafa3b3bc2fc8d0624fbaf3e8ba
Danny Vasquez <danny@dannys-macbook-pro.local>
2016-01-13 13:46:40 -0600

    Initial Commit

---
+++ Twitterrizzer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitterrizzer
+//
+//  Created by Danny Vasquez on 1/13/16.
+//  Copyright © 2016 Danny Vasquez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
